f136896dfdfd7355ab289ffc4cd5e7c637d503e4
Alexei <uleecsei@gmail.com>
2020-05-13 22:55:21 +0300

    path check

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const connectConfig = 'mongodb+srv://uleecsei:4n8qMgU2bZXFxz9@cluster0-dz2dg.mongodb.net/test?retryWrites=true&w=majority';
-const todoRoute = require('./server/routes/todo');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const connectConfig =
+  "mongodb+srv://uleecsei:4n8qMgU2bZXFxz9@cluster0-dz2dg.mongodb.net/test?retryWrites=true&w=majority";
+const todoRoute = require("./server/routes/todo");
 
 app.use(bodyParser.json());
 app.use(cors());
 
 mongoose.connect(connectConfig, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
-app.use('/api/todos', todoRoute);
+app.use("/api/todos", todoRoute);
 
+app.use(express.static(__dirname + "/dist/ToDo-app"));
 
-app.use(express.static(__dirname + '/dist/ToDo-app'));
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/ToDo-app/index.html'))
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/ToDo-app/index.html"));
 });
 
-
-app.listen(process.env.PORT || 8080);
+const server = app.listen(process.env.PORT || 8080, () => {
+  console.log(server.address());
+});
